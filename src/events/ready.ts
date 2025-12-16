2c428063ed4a8560e2c231dca5ae2536310dfacb
charlie-1126 <charlie869599@gmail.com>
2025-12-17 08:06:53 +0900

    feat: activity update

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
-import { Events, Client } from 'discord.js';
-import { startScheduler } from '../services/scheduler';
+import { Events, Client } from "discord.js";
+import { startScheduler } from "../services/scheduler";
 
 export default {
     name: Events.ClientReady,
@@ -7,12 +7,12 @@ export default {
     async execute(client: Client) {
         console.log(`${client.user?.tag}으로 로그인 성공!`);
         console.log(`봇이 ${client.guilds.cache.size}개의 서버에서 실행 중`);
-		startScheduler(client);
+        startScheduler(client);
 
         // 봇 상태 설정
         client.user?.setPresence({
-            activities: [{ name: '디스코드', type: 0 }],
-            status: 'online',
+            activities: [{ name: "HSR 로테이션 봇", type: 4 }],
+            status: "online",
         });
-    }
+    },
 };
